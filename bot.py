f5069f99f802b8eb7d4ceb99298259e7f658fa03
riquelmedmr <73765375+riquelmedmr@users.noreply.github.com>
2025-10-20 05:45:43 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -291,6 +291,7 @@ def home():
             <div class="info">
                 Bot Telegram para remoção de duplicatas<br>
                 Adicione-me ao seu grupo!
+                https://t.me/duplicatacleanerbot
             </div>
         </div>
     </body>
